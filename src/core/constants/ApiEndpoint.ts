ba4e6ae1dae11c39cd70742976bbba31d0207270
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-11-14 13:10:08 +0800

    fix(api): add /model/ path segment to all API endpoints
    - Add /model/ between /list or /view and resource type
    - Fixes 'Parameter Model is Missing' API error
    - Required by BPS WebAPI for all endpoints

--- src/core/constants/ApiEndpoint.ts
+++ src/core/constants/ApiEndpoint.ts
@@ -3,79 +3,79 @@
  */
 export const ApiEndpoint = {
   // Domain endpoints
-  DOMAIN_LIST: '/list/domain',
-  DOMAIN_VIEW: '/view/domain',
+  DOMAIN_LIST: '/list/model/domain',
+  DOMAIN_VIEW: '/view/model/domain',
 
   // Publication endpoints
-  PUBLICATION_LIST: '/list/publication',
-  PUBLICATION_VIEW: '/view/publication',
+  PUBLICATION_LIST: '/list/model/publication',
+  PUBLICATION_VIEW: '/view/model/publication',
 
   // Infographic endpoints
-  INFOGRAPHIC_LIST: '/list/infographic',
-  INFOGRAPHIC_VIEW: '/view/infographic',
+  INFOGRAPHIC_LIST: '/list/model/infographic',
+  INFOGRAPHIC_VIEW: '/view/model/infographic',
 
   // Static table endpoints
-  STATIC_TABLE_LIST: '/list/statictable',
-  STATIC_TABLE_VIEW: '/view/statictable',
+  STATIC_TABLE_LIST: '/list/model/statictable',
+  STATIC_TABLE_VIEW: '/view/model/statictable',
 
   // News endpoints
-  NEWS_LIST: '/list/news',
-  NEWS_VIEW: '/view/news',
+  NEWS_LIST: '/list/model/news',
+  NEWS_VIEW: '/view/model/news',
 
   // News category endpoints
-  NEWS_CATEGORY_LIST: '/list/newscategory',
-  NEWS_CATEGORY_VIEW: '/view/newscategory',
+  NEWS_CATEGORY_LIST: '/list/model/newscategory',
+  NEWS_CATEGORY_VIEW: '/view/model/newscategory',
 
   // Press release endpoints
-  PRESS_RELEASE_LIST: '/list/pressrelease',
-  PRESS_RELEASE_VIEW: '/view/pressrelease',
+  PRESS_RELEASE_LIST: '/list/model/pressrelease',
+  PRESS_RELEASE_VIEW: '/view/model/pressrelease',
 
   // Subject endpoints
-  SUBJECT_LIST: '/list/subject',
-  SUBJECT_VIEW: '/view/subject',
+  SUBJECT_LIST: '/list/model/subject',
+  SUBJECT_VIEW: '/view/model/subject',
 
   // Subject category endpoints
-  SUBJECT_CATEGORY_LIST: '/list/subjectcat',
-  SUBJECT_CATEGORY_VIEW: '/view/subjectcat',
+  SUBJECT_CATEGORY_LIST: '/list/model/subjectcat',
+  SUBJECT_CATEGORY_VIEW: '/view/model/subjectcat',
 
   // Strategic indicator endpoints
-  STRATEGIC_INDICATOR_LIST: '/list/stratind',
-  STRATEGIC_INDICATOR_VIEW: '/view/stratind',
+  STRATEGIC_INDICATOR_LIST: '/list/model/indicators',
+  STRATEGIC_INDICATOR_VIEW: '/view/model/indicators',
 
   // Variable endpoints
-  VARIABLE_LIST: '/list/var',
-  VARIABLE_VIEW: '/view/var',
+  VARIABLE_LIST: '/list/model/var',
+  VARIABLE_VIEW: '/view/model/var',
 
   // Vertical variable endpoints
-  VERTICAL_VARIABLE_LIST: '/list/vervar',
-  VERTICAL_VARIABLE_VIEW: '/view/vervar',
+  VERTICAL_VARIABLE_LIST: '/list/model/vervar',
+  VERTICAL_VARIABLE_VIEW: '/view/model/vervar',
 
   // Unit endpoints
-  UNIT_LIST: '/list/unit',
-  UNIT_VIEW: '/view/unit',
+  UNIT_LIST: '/list/model/unit',
+  UNIT_VIEW: '/view/model/unit',
 
   // Period endpoints
-  PERIOD_LIST: '/list/period',
-  PERIOD_VIEW: '/view/period',
+  PERIOD_LIST: '/list/model/period',
+  PERIOD_VIEW: '/view/model/period',
 
   // Derived period endpoints
-  DERIVED_PERIOD_LIST: '/list/turth',
-  DERIVED_PERIOD_VIEW: '/view/turth',
+  DERIVED_PERIOD_LIST: '/list/model/turth',
+  DERIVED_PERIOD_VIEW: '/view/model/turth',
 
   // Derived variable endpoints
-  DERIVED_VARIABLE_LIST: '/list/turvar',
-  DERIVED_VARIABLE_VIEW: '/view/turvar',
+  DERIVED_VARIABLE_LIST: '/list/model/turvar',
+  DERIVED_VARIABLE_VIEW: '/view/model/turvar',
 
   // Dynamic table endpoints
-  DYNAMIC_TABLE_LIST: '/list/dynamictable',
+  DYNAMIC_TABLE_LIST: '/list/model/dynamictable',
 
   // Statistic classification endpoints
-  STATISTIC_CLASSIFICATION_LIST: '/list/kbli',
-  STATISTIC_CLASSIFICATION_VIEW: '/view/kbli',
+  STATISTIC_CLASSIFICATION_LIST: '/list/model/kbli',
+  STATISTIC_CLASSIFICATION_VIEW: '/view/model/kbli',
 
   // Census endpoints
-  CENSUS_LIST: '/list/sensus',
-  CENSUS_VIEW: '/view/sensus',
+  CENSUS_LIST: '/list/model/sensus',
+  CENSUS_VIEW: '/view/model/sensus',
 
   // Trade endpoints
   TRADE: '/dataexim',
